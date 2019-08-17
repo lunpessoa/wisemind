d86747faece3309daf7ccfbfaaa545f802e570e7
lunpessoa <luan.pessoa.393@hotmail.com>
2019-08-16 22:12:11 -0300

    importacao inicial

---
+++ index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+
+  <?php include('menu.html'); ?>
+
+  <section id="carouselSite" class="carousel slide" style="z-index: 0" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselSite" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselSite" data-slide-to="1"></li>
+      <li data-target="#carouselSite" data-slide-to="2"></li>
+    </ol>
+    <section class="carousel-inner">
+      <section class="carousel-item active">
+        <img src="img/back2.jpg" class="img-fluid d-block">
+        <section class="carousel-caption d-none d-md-block text-dark">
+          <h3>Rainbow Six</h3>
+          <p>Warden</p>
+        </section>
+      </section>
+      <section class="carousel-item">
+        <img src="img/back.png" class="img-fluid d-block">
+        <section class="carousel-caption d-none d-md-block text-light">
+          <h3>Wise Mind</h3>
+          <p>Aprenda, Dedique-se e Conquiste</p>
+        </section>
+      </section>
+      <section class="carousel-item">
+        <img src="img/back3.png" class="img-fluid d-block">
+        <section class="carousel-caption d-none d-md-block text-light">
+          <h3>Rainbow Six</h3>
+          <p>Nokk</p>
+        </section>
+      </section>
+    </section>
+    <a class="carousel-control-prev" href="#carouselSite" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+      <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselSite" role="button" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+      <span class="sr-only">Avançar</span>
+    </a>
+  </section>
+
+  <section class="container-fluid mt-5">
+    <section class="row">
+      <section class="col-sm-12">
+        <section id="carouselCard" class="carousel slide">
+
+          <section class="carousel-inner">
+            <section class="carousel-item active">
+              <section class="container">
+                <section class="row">
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                </section>
+              </section>
+            </section>
+
+            <section class="carousel-item">
+              <section class="container">
+                <section class="row">
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                  <section class="col-sm-4 justify-content-center d-flex">
+                    <section class="card Cards" style="width: 350px">
+                      <img src="img/back.png" class="card-img-top" alt="">
+                      <section class="card-body">
+                        <h5 class="card-title">Card</h5>
+                        <p class="card-text">Lajajdçsjdçajdçlajdçlajdçajdçladjçadowjdçawsjadsdaçjdwwaoj</p>
+                      </section>
+                    </section>
+                  </section>
+
+                </section>
+              </section>
+            </section>
+          </section>
+
+          <a class="carousel-control-prev" href="#carouselCard" role="button" data-slide="prev">
+            <i style="font-size:30px; color:#000;" class="fas fa-angle-double-left"></i>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselCard" role="button" data-slide="next">
+            <i style="font-size:30px; color:#000;" class="fas fa-angle-double-right"></i>
+            <span class="sr-only">Previous</span>
+          </a>
+
+        </section>
+      </section>
+    </section>
+  </section>
+</body>
+
+</html>
---
+++ js/javinha.js
@@ -0,0 +1,14 @@
+var zero = 0;
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+            $('.navbarMenuItens').toggleClass('hide', $(window).scrollTop() > zero);
+    })
+})
+
+var zero = 0;
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+            $('.fundoLogo').toggleClass('svgRetrat', $(window).scrollTop() > zero);
+            $('.svgLogo').toggleClass('logoRetrat', $(window).scrollTop() > zero);
+    })
+})
