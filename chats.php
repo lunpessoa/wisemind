009cbfe18c1e0866b1cdf17c9ce4b28ab82cc946
lunpessoa <luan.pessoa.393@hotmail.com>
2019-08-23 22:05:38 -0300

    Gif_slide

--- chats.php
+++ chats.php
@@ -23,7 +23,7 @@
 			<section class="col-12 col-md-4">
 				<a class="btn" href="#">
 					<section class="card">
-						<img src="img/fundo.png" class="card-img-top" alt="...">
+						<img src="img/slide_amarelo.gif" class="card-img-top" alt="...">
 						<section class="card-body text-center">
 							<h3 class="card-title h3"><b><em>Medicina</em></b></h3>
 						</section>
