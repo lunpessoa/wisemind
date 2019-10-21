3a632113dfc6aa15ab7c560c39e580ef903278ca
Antonio Souza <47275237+Bartolomeuu@users.noreply.github.com>
2019-10-21 01:43:58 -0300

    cadastro

--- assets/sala.php
+++ assets/sala.php
@@ -9,16 +9,16 @@
         $data = array_reverse($data);
         $data = implode ('-', $data);
         
-                $sqlinserir = ('insert into chat(area,num_participantes,data_criacao) values ('.$nome.',0,"'.$data.'")');
+                $sqlinserir = ('insert into chat(area,num_participantes,data_criacao) values ("'.$nome.'",0,"'.$data.'")');
                 $inserir=mysqli_query($conexao,$sqlinserir) or die (mysqli_error($conexao));
 
-                if($inserir1){ 
+                if($inserir){ 
                     
                     echo('<script>alert("Inserido com sucesso")
                     window.location.href = "../index.php";</script>');//cadastro com sucesso
                     
                     
-                    }
+                    
 
                 }else {
                     echo('<script>alert("erro")
--- chats.php
+++ chats.php
@@ -68,6 +68,7 @@
 					</section>
 				</a>
 			</section>
+		</section>
 		
 		');
 		}
