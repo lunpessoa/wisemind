8e1cd8680186da48343724bf5bebef7f638252fe
lunpessoa <luan.pessoa.393@hotmail.com>
2019-10-28 03:42:14 -0300

    wiser

--- compra1.php
+++ compra1.php
@@ -19,30 +19,73 @@
 </head>
 
 <body id="body">
-	<section class="container-fluid mb-5 plano">
-		<div class="row align-content-center" style="height:400px;">
-			<div class="col-6">
-				<p class="text-center">
-					<label class="h1 d-block mb-3">APENAS R$39,99</label>
-					<i class="fas fa-brain display-3 d-block text-warning"></i>
-					<label class="h2 d-block mt-3">PLANO WISER </label>
-				</p>
+	<section class="img-fluid backimage text-center" alt="Responsive image">
+		<img src="img/wiserelement.png" class="position-relative wiserelement" alt="">
+	</section>
+
+	<section class="container-fluid p-5 w-100 plano align-items-center d-flex" style="z-index:0;">
+		<div class="container">
+			<!--first section-->
+			<div class="row align-items-center how-it-works d-flex">
+				<div class="col-2 text-center d-inline-flex justify-content-center align-items-center">
+					<div class="circle font-weight-bold"><i class="fas fa-dollar-sign"></i></div>
+				</div>
+				<div class="col-6 font-lobster">
+					<h1>Apenas R$ 39,90</h1>
+				</div>
 			</div>
-			<div class="col-6">
-				<div id="materiais">Materiais exclusivos</div>
-				<div id="curriculo">Aprenda a montar um bom curriculo</div>
-				<div id="vagas">Acesso a vagas de emprego</div>
+			<!--path between 1-2-->
+			<div class="row timeline">
+				<div class="col-2">
+					<div class="corner top-right"></div>
+				</div>
+				<div class="col-8">
+					<hr />
+				</div>
+				<div class="col-2">
+					<div class="corner left-bottom"></div>
+				</div>
+			</div>
+			<!--second section-->
+			<div class="row align-items-center justify-content-end how-it-works d-flex">
+				<div class="col-6 text-right font-lobster">
+					<h1>Conteúdos Adicionais</h1>
+				</div>
+				<div class="col-2 text-center full d-inline-flex justify-content-center align-items-center">
+					<div class="circle font-weight-bold"><i class="fas fa-plus"></i></div>
+				</div>
+			</div>
+			<!--path between 2-3-->
+			<div class="row timeline">
+				<div class="col-2">
+					<div class="corner right-bottom"></div>
+				</div>
+				<div class="col-8">
+					<hr />
+				</div>
+				<div class="col-2">
+					<div class="corner top-left"></div>
+				</div>
+			</div>
+			<!--third section-->
+			<div class="row align-items-center how-it-works d-flex">
+				<div class="col-2 text-center top d-inline-flex justify-content-center align-items-center">
+					<div class="circle font-weight-bold"><i class="fas fa-brain"></i></div>
+				</div>
+				<div class="col-6 font-lobster">
+					<h1>Melhor Experiência</h1>
+				</div>
 			</div>
 		</div>
 	</section>
 
-
-	<section class="container-fluid formulario p-5 text-light">
+	<section class="container-fluid formulario p-5 text-light" style="z-index:0; margin-top:500px;">
 		<section class="container">
 			<section class="row justify-content-center mb-2">
 				<section class="col-md-12">
 
-					<label class="h5 my-3 font-italic">Para concluir o pagamento, precisamos dos seguintes dados
+					<label class="h5 my-3 font-italic pt-5">Para concluir o pagamento, precisamos dos seguintes
+						dados
 						pessoais para efetuarmos a geração do
 						boleto.</label>
 				</section>
