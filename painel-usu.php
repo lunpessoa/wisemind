22774265963c90083f4c55d94de8521832ec3dbf
Antonio Souza <47275237+Bartolomeuu@users.noreply.github.com>
2019-11-15 16:44:40 -0200

    php

--- painel-usu.php
+++ painel-usu.php
@@ -551,24 +551,20 @@
 						<table class="table table-borderless text-light">
 							<thead>
 								<tr>
-									<th scope="col">#</th>
 									<th scope="col">Área</th>
 									<th scope="col">Profissão</th>
 								</tr>
 							</thead>
 							<tbody>
 								<tr>
-									<th scope="row">1</th>
 									<td>Mark</td>
 									<td>Otto</td>
 								</tr>
 								<tr>
-									<th scope="row">2</th>
 									<td>Jacob</td>
 									<td>Thornton</td>
 								</tr>
 								<tr>
-									<th scope="row">3</th>
 									<td>Larry</td>
 									<td>the Bird</td>
 								</tr>
