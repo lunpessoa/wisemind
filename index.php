ba9613a5448e11c7312fcfdb4ef4867d190fa6ec
lunpessoa <luan.pessoa.393@hotmail.com>
2019-11-15 15:34:29 -0300

    index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION)) {
 </head>
 
 <body>
-  
+
 
   <?php include('menu.php');?>
 
@@ -65,46 +65,46 @@ if (!isset($_SESSION)) {
           <div class="col-md-6 col-12 p-0 d-inline-flex" style="background-color:#fac215">
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top"
               title="Administração, Economia e Negócios" style="background-color:#b6151d">
-              <a class="nav-link light-red p-0" href="adm.php"><i class="fas fa-pencil-alt iconsArea"
-                  style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="adm.php"><i class="fas fa-pencil-alt text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Ciências Sociais"
               style="background-color:#d21b3a">
-              <a class="nav-link light-red" href="sociais.php"><i class="fas fa-brain iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="sociais.php"><i class="fas fa-brain text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Comunicação e Mídia"
               style="background-color:#f35b07">
-              <a class="nav-link" href="comunicacao.php"><i class="far fa-newspaper iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="comunicacao.php"><i class="far fa-newspaper  text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Design e Arte"
               style="background-color:#fb762b">
-              <a class="nav-link" href="artistico.php"><i class="fas fa-paint-brush iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="artistico.php"><i class="fas fa-paint-brush text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Direito"
               style="background-color:#fac215">
-              <a class="nav-link" href="direito.php"><i class="fas fa-balance-scale iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="direito.php"><i class="fas fa-balance-scale text-light"></i></a>
             </li>
           </div>
           <div class="col-md-6 col-12 p-0 d-inline-flex">
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Educação"
               style="background-color:#a8d12d">
-              <a class="nav-link" href="educacao.php"><i class="fas fa-chalkboard-teacher iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link  iconsArea" href="educacao.php"><i
+                  class="fas fa-chalkboard-teacher text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Engenharia e Arquitetura"
               style="background-color:#4aaf75">
-              <a class="nav-link" href="engenharia.php"><i class="fas fa-pencil-ruler iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link  iconsArea" href="engenharia.php"><i class="fas fa-pencil-ruler text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Informação e Tecnologia"
               style="background-color:#268cb4">
-              <a class="nav-link" href="info.php"><i class="fas fa-laptop-code iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link  iconsArea" href="info.php"><i class="fas fa-laptop-code text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Saúde"
               style="background-color:#1469bc">
-              <a class="nav-link" href="saude.php"><i class="fas fa-heartbeat iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link  iconsArea" href="saude.php"><i class="fas fa-heartbeat text-light"></i></a>
             </li>
             <li class="nav-item areasLinks" data-toggle="tooltip" data-placement="top" title="Turismo e Gastronomia"
               style="background-color:#831168">
-              <a class="nav-link p-0" href="turismo.php"><i class="fas fa-utensils iconsArea" style="color:#fff"></i></a>
+              <a class="nav-link iconsArea" href="turismo.php"><i class="fas fa-utensils text-light"></i></a>
             </li>
           </div>
         </div>
@@ -113,11 +113,15 @@ if (!isset($_SESSION)) {
   </nav>
 
 
-  <section class="container-fluid p-0">
+  <section class="container-fluid p-0 mb-5" style="margin-top:100px">
     <div class="row mr-0 ml-0">
       <div class="col-md-6 col-12 text-left p-5">
-        <h3 class="display-3 mt-5"><b><em><i class="fas fa-user-graduate"></i> Estudante</em></b></h3>
-        <p class="text-left h4 mr-5 mt-3">Na plataforma WiseMind o estudante tem a oportunidade de se comunicar com
+        <h3 class="display-3 mt-5 font-lobster justify-content-center align-items-end d-flex">
+          <p>Estudante</p>
+          <i class="fas fa-user-graduate position-absolute text-warning icon-info"></i>
+        </h3>
+        <p class="lead mr-5 mt-3 text-justify" style="font-size:25px">Na plataforma WiseMind o estudante tem a
+          oportunidade de se comunicar com
           profissionais ja consolidados no
           mercado de trabalho, descobrir novas profissões, ter acesso a materias de apoio, chances de ingressar no
           mercado e muito mais de forma rápida e tudo em um único lugar.</p>
@@ -131,23 +135,26 @@ if (!isset($_SESSION)) {
     </div>
   </section>
 
-  <section class="container-fluid justify-content-center d-flex align-items-center"
-    style="height:200px; background-image: url('img/fundo.png'); background-attachment: fixed;">
-    <h1 class="display-3 text-primary text-uppercase "><b><em><i class="fas fa-cogs"></i> Plataforma WiseMind</em></b>
+  <section class="container-fluid justify-content-center d-flex align-items-center plataform-back">
+    <h1 class="display-3 text-primary text-uppercase font-roboto"><b><em><i class="fas fa-cogs"></i> Plataforma
+          WiseMind</em></b>
     </h1>
   </section>
 
-  <section class="container-fluid p-0">
+  <section class="container-fluid p-0" style="margin-top:100px">
     <div class="row mr-0 ml-0">
       <div class="col-md-6 col-12 p-0">
         <div class="profissional">
-          <aside class="content_profissional">
-          </aside>
+          <aside class="content_profissional"></aside>
         </div>
       </div>
       <div class="col-md-6 col-12 text-right p-5">
-        <h3 class="display-3 text-dark mr-5 mt-5"><b><em>Profissional <i class="fas fa-user-tie"></i></em></b></h3>
-        <p class="text-right h4 mr-5 mt-3">O profissional é muito valorizado em nossa plataforma, pois é ele o
+        <h3 class="display-3 mt-5 font-lobster justify-content-center align-items-end d-flex">
+          <p>Profissional</p>
+          <i class="fas fa-user-tie position-absolute text-warning icon-info"></i>
+        </h3>
+        <p class="lead mr-5 mt-3 text-justify" style="font-size:25px">O profissional é muito valorizado em nossa
+          plataforma, pois é ele o
           responsável por boa parte das realizações do nosso serviço, além de aumentar o seu horizonte de conhecimento
           com outros profissionais e com os jovens, o profisional tem a oportunidade e encontrar e ser indicado a vagas
           de emprego.</p>
@@ -155,21 +162,23 @@ if (!isset($_SESSION)) {
     </div>
   </section>
 
-  <section class="container">
+  <section class="container" style="margin-top:100px">
     <hr>
   </section>
 
-  <section class="container text-center mt-5">
+  <section class="container text-center" style="margin-top:100px">
     <section class="row mb-5">
       <section class="col-12">
-        <h1 class="display-2"><i class="fas fa-users text-warning"></i> Sobre Nós</h1>
-        <p>Aprenda, dedique-se e Conquiste</p>
+        <h1 class="display-1 font-lobster">
+          <i class="fas fa-users text-dark"></i>
+          Sobre Nós
+        </h1>
+        <p class="font-italic font-roboto h5">Aprenda, dedique-se e Conquiste</p>
       </section>
     </section>
   </section>
 
-  <section class="container-fluid pb-5"
-    style="background-image: url('img/fundo_azul.png'); background-attachment: fixed;">
+  <section class="container-fluid sobre-back">
     <section class="container  d-flex">
       <p class="h4 text-center text-light mt-5" style="line-height: 50px;">A plataforma Wise Mind surgiu a partir da
         análise de necessidades atuais, existe uma crise que complica a vida de quem quer
