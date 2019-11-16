e0174ce0dfdcf31525d3c1e52eb2d39cddb2e405
Antonio Souza <47275237+Bartolomeuu@users.noreply.github.com>
2019-11-15 22:04:11 -0200

    pedido

--- assets/compra-config.php
+++ assets/compra-config.php
@@ -2,18 +2,17 @@
     session_start();
     include('../assets/conexao.php');
     if(isset($_POST["Enviar"])){
-        $sql =('select Nome, Sobrenome from usuarios where id_usuarios='.$_SESSION["id_user"].';');
-        $sql_play=mysqli_query($conexao, $sql);
-        while($con1=mysqli_fetch_array($sql_play)){
-            $nome = $con1['Nome'];
-            $sobrenome = $con1['Sobrenome'];
-        }
+        $sql=('select * from usuarios where id_usuario = '. $_SESSION["id_user"].';');
+		$resul=mysqli_query($conexao, $sql);
+        $con1=mysqli_fetch_array($resul);
+        $nome = $con1['Nome'];
+        $sobrenome = $con1['Sobrenome'];
 
-        $sql2 =('select Preco from planos where id_plano=1;');
+        $sql2 =('select * from planos where id_plano=1;');
         $sql_show2=mysqli_query($conexao, $sql2);
-        while($con2=mysqli_fetch_array($sql_show2)){
-            $valor=$con2['Preco'];
-        }
+        $con2=mysqli_fetch_array($sql_show2);
+        $valor=$con2['Preco'];
+        
 
         $_SESSION['valor'] = $valor;
         $_SESSION['endereco'] = $rua = $_POST['rua'];
@@ -27,10 +26,16 @@
         $dias_de_prazo = 5;
         $data_venc = date("Y-m-d", time() + ($dias_de_prazo * 86400));
         $data = date("Y-m-d");
-        
-        //$sql_inserir=('insert into pedido(Data_Compra, Data_Validade, Renovacao, id_estuda, id_plano) values('.$data.', '.$data_venc.', "N",'.$_SESSION['id_user'].', 1;');
-        //mysqli_query($conexao, $sql_inserir) or die (mysqli_error($conexao));
 
-        header("Location: ../compra/boleto/boleto_bb.php");
+        echo($data_venc.'<br>');
+        echo($data.'<br>');
+        
+        $sqlinserir = ('insert into pedido (Data_Compra, Data_Validade, Renovacao, id_estuda, id_plano) values
+         ("'.$data.'", "'.$data_venc.'", "N",'.$_SESSION["id_user"].', 1);');
+        $inserir=mysqli_query($conexao,$sqlinserir) or die (mysqli_error($conexao));
+
+        if($inserir){
+            header("Location: ../compra/boleto/boleto_bb.php");
+        }
     }
 ?>
