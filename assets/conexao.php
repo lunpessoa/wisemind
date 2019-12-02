7128f49beeae82a8884579ed5a2326da9ff176c5
lunpessoa <luan.pessoa.393@hotmail.com>
2019-12-02 01:28:14 -0300

    conexao

--- assets/conexao.php
+++ assets/conexao.php
@@ -1,3 +1,4 @@
 <?php
-    $conexao=mysqli_connect("localhost","root","","wisemind") or die ("Falha de conexão".mysqli_error())
+    $conexao=mysqli_connect("localhost","root","","wisemind") or die ("Falha de conexão".mysqli_error());
+    mysql_set_charset('utf8',$conexao);
 ?>
